6a0ceb6cea01b07065637c6ca5fca182fdabc830
chunmeng fan <1416906555@qq.com>
2022-06-20 17:53:01 +0800

    将kafka生产者改为单例模式

--- web/middleware/postSearchLog.go
+++ web/middleware/postSearchLog.go
@@ -7,16 +7,10 @@ import (
 	"github.com/gin-gonic/gin/binding"
 	"log"
 	"strconv"
-	"strings"
 	"time"
 )
 
-var (
-	brokers = []string{"127.0.0.1:9092"}
-	Topic   = "Test"
-)
-
-func PostBySync() gin.HandlerFunc {
+func PostBySync(producer *SingletonProducer) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		t := strconv.FormatInt(time.Now().Unix(), 10)
 		c.Next()
@@ -27,11 +21,11 @@ func PostBySync() gin.HandlerFunc {
 		if err := c.ShouldBindBodyWith(sle, binding.JSON); err != nil {
 			log.Printf("err %v", err)
 		}
-		log.Printf("Kafka brokers: %s", strings.Join(brokers, ", "))
-		producer := newSyncProducer(brokers)
-		defer producer.Close()
-		partition, offset, err := producer.SendMessage(&sarama.ProducerMessage{
-			Topic: Topic,
+
+		//log.Printf("Kafka brokers: %s", strings.Join(producer.BrokerList, ", "))
+		log.Println(producer.syncProducer)
+		partition, offset, err := producer.GetSyncProducer().SendMessage(&sarama.ProducerMessage{
+			Topic: producer.Topic,
 			Value: sle,
 		})
 		if err != nil {
@@ -41,81 +35,71 @@ func PostBySync() gin.HandlerFunc {
 		}
 	}
 }
-func PostByAsync() gin.HandlerFunc {
+func PostByAsync(producer *SingletonProducer) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		//	处理逻辑
-		log.Printf("Kafka brokers: %s", strings.Join(brokers, ", "))
-		//	如果是第一次，初始化
-
-		//	把搜索请求异步发送给kafka
+		t := strconv.FormatInt(time.Now().Unix(), 10)
+		c.Next()
 		var sle = &searchLogEntry{
-			Time:           strconv.FormatInt(time.Now().Unix(), 10),
+			Time:           t,
 			Identification: c.ClientIP(),
 		}
 		if err := c.ShouldBind(sle); err != nil {
 			log.Printf("paras bind err: %v", err)
 		}
-		//new producer?
-		producer := newAsyncProducer(brokers)
-		//producer
-		producer.Input() <- &sarama.ProducerMessage{
-			Topic: Topic,
+		producer.GetAsyncProducer().Input() <- &sarama.ProducerMessage{
+			Topic: producer.Topic,
 			Key:   sarama.StringEncoder(sle.Identification),
 			Value: sle,
 		}
-		c.Next()
 	}
 
 }
 
-func newAsyncProducer(brokerList []string) sarama.AsyncProducer {
-	// For the search log, we are looking for AP semantics, with high throughput.
-	// By creating batches of compressed messages, we reduce network I/O at a cost of more latency.
-	config := sarama.NewConfig()
-	//tlsConfig := createTlsConfiguration()
-	//if tlsConfig != nil {
-	//	config.Net.TLS.Enable = true
-	//	config.Net.TLS.Config = tlsConfig
-	//}
-	config.Producer.RequiredAcks = sarama.WaitForLocal       // Only wait for the leader to ack
-	config.Producer.Compression = sarama.CompressionSnappy   // Compress messages
-	config.Producer.Flush.Frequency = 500 * time.Millisecond // Flush batches every 500ms
+type SingletonProducer struct {
+	syncProducer  sarama.SyncProducer
+	asyncProducer sarama.AsyncProducer
+	BrokerList    []string
+	Topic         string
+}
 
-	producer, err := sarama.NewAsyncProducer(brokerList, config)
+func (sp *SingletonProducer) GetSyncProducer() sarama.SyncProducer {
+	if sp.syncProducer == nil {
+		sp.syncProducer = sp.newSyncProducer()
+	}
+	return sp.syncProducer
+}
+func (sp *SingletonProducer) GetAsyncProducer() sarama.AsyncProducer {
+	if sp.asyncProducer == nil {
+		sp.asyncProducer = sp.newAsyncProducer()
+	}
+	return sp.asyncProducer
+}
+
+func (sp *SingletonProducer) newSyncProducer() sarama.SyncProducer {
+	config := sarama.NewConfig()
+	config.Producer.RequiredAcks = sarama.WaitForAll // Wait for all in-sync replicas to ack the message
+	config.Producer.Retry.Max = 10                   // Retry up to 10 times to produce the message
+	config.Producer.Return.Successes = true
+	producer, err := sarama.NewSyncProducer(sp.BrokerList, config)
 	if err != nil {
 		log.Fatalln("Failed to start Sarama producer:", err)
 	}
-	// We will just log to STDOUT if we're not able to produce messages.
-	// Note: messages will only be returned here after all retry attempts are exhausted.
+	return producer
+}
+func (sp *SingletonProducer) newAsyncProducer() sarama.AsyncProducer {
+	config := sarama.NewConfig()
+	config.Producer.RequiredAcks = sarama.WaitForLocal       // Only wait for the leader to ack
+	config.Producer.Compression = sarama.CompressionSnappy   // Compress messages
+	config.Producer.Flush.Frequency = 500 * time.Millisecond // Flush batches every 500ms
+	producer, err := sarama.NewAsyncProducer(sp.BrokerList, config)
+	if err != nil {
+		log.Fatalln("Failed to start Sarama producer:", err)
+	}
 	go func() {
 		for err := range producer.Errors() {
 			log.Println("Failed to write search log entry:", err)
 		}
 	}()
-	return producer
-}
-
-func newSyncProducer(brokerList []string) sarama.SyncProducer {
-	config := sarama.NewConfig()
-	config.Producer.RequiredAcks = sarama.WaitForAll // Wait for all in-sync replicas to ack the message
-	config.Producer.Retry.Max = 10                   // Retry up to 10 times to produce the message
-	config.Producer.Return.Successes = true
-	//tlsConfig := createTlsConfiguration()
-	//if tlsConfig != nil {
-	//	config.Net.TLS.Config = tlsConfig
-	//	config.Net.TLS.Enable = true
-	//}
-
-	// On the broker side, you may want to change the following settings to get
-	// stronger consistency guarantees:
-	// - For your broker, set `unclean.leader.election.enable` to false
-	// - For the topic, you could increase `min.insync.replicas`.
-
-	producer, err := sarama.NewSyncProducer(brokerList, config)
-	if err != nil {
-		log.Fatalln("Failed to start Sarama producer:", err)
-	}
-
 	return producer
 }
 
--- web/router/base.go
+++ web/router/base.go
@@ -6,9 +6,18 @@ import (
 	"go-search/web/middleware"
 )
 
+var (
+	brokers = []string{"127.0.0.1:9092"}
+	topic   = "Test"
+)
+
 // InitBaseRouter 基础管理路由
 func InitBaseRouter(Router *gin.RouterGroup) {
-	Router.Use(middleware.PostBySync())
+	var singletonProducer = &middleware.SingletonProducer{
+		BrokerList: brokers,
+		Topic:      topic,
+	}
+	Router.Use(middleware.PostBySync(singletonProducer))
 	BaseRouter := Router.Group("")
 	{
 		BaseRouter.GET("/", controller.Welcome)
