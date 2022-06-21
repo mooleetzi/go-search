dda2931d0bcd29139f5420c99af6252c650456fa
chunmeng fan <1416906555@qq.com>
2022-06-21 14:55:55 +0800

    pprof

--- main.go
+++ main.go
@@ -2,10 +2,21 @@ package main
 
 import (
 	"go-search/core"
+	"go-search/log"
+	"net/http"
+	_ "net/http/pprof"
+	"runtime"
 )
 
 func main() {
 
+	go func() {
+		runtime.SetBlockProfileRate(1) // 开启对阻塞操作的跟踪，block
+		runtime.SetMutexProfileFraction(1)
+		if err := http.ListenAndServe(":6060", nil); err != nil {
+			log.Fatal(err)
+		}
+	}()
 	// 初始化容器和参数解析
 	core.Initialize()
 }
